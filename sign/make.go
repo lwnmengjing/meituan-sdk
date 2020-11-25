8505f8cb55abe674cd49aa8315185d5249f9ab7a
linwenxiang <991154416@qq.com>
2020-11-25 23:40:57 +0800

    增加scheme参数

--- sign/make.go
+++ sign/make.go
@@ -3,14 +3,16 @@ package sign
 import "net/url"
 
 // Make 生成签名
-func Make(secret, host, path string, values Values) string {
-	return make(secret, host, path, values)
+func Make(secret, scheme, host, path string, values Values) string {
+	return make(secret, scheme, host, path, values)
 }
 
-func make(secret, host, path string, values Values) string {
+// make 生成签名(私有)
+func make(secret, scheme, host, path string, values Values) string {
 	u := &url.URL{
-		Host: host,
-		Path: path,
+		Scheme: scheme,
+		Host:   host,
+		Path:   path,
 	}
 	u.RawQuery = values.Encode()
 	sign := md5V(u.String() + secret)
